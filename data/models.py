0cd2b29d8362891a1e3f578a16b29a9b592b76cc
Andrew Stead <andrew@andrewstead.co.uk>
2018-10-16 17:09:55 +0000

    Added country data to default database.

--- data/models.py
+++ data/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Sport(models.Model):
     objects = models.Manager()
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25, unique=True)
     icon = models.ImageField(upload_to="images/sports", blank=True, null=True)
    
     def __str__(self):
@@ -13,9 +13,10 @@ class Sport(models.Model):
         
 class Country(models.Model):
     objects = models.Manager()
-    name = models.CharField(max_length=100)
-    short_name = models.CharField(max_length=25)
-    abbreviation = models.CharField(max_length=25)
+    name = models.CharField(max_length=100, unique=True)
+    short_name = models.CharField(max_length=25, unique=True)
+    abbreviation = models.CharField(max_length=25, unique=True)
+    flag = models.ImageField(upload_to="images/countries", blank=True, null=True)
    
     def __str__(self):
         return self.name

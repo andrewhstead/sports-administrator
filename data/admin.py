d04a9a958f866f0239462ab5f41b7caa88e85947
Andrew Stead <andrew@andrewstead.co.uk>
2018-10-19 16:59:27 +0000

    Added facility to add editions of a competition to database.

--- data/admin.py
+++ data/admin.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Sport, Country, Competition
+from .models import Sport, Country, Competition, Season, Edition
 
 # Register your models here.
 admin.site.register(Sport)
 admin.site.register(Country)
 admin.site.register(Competition)
+admin.site.register(Season)
+admin.site.register(Edition)
--- data/models.py
+++ data/models.py
@@ -1,6 +1,12 @@
 from __future__ import unicode_literals
 from django.db import models
 
+# Options for different types of season, to set as calendar year or multi-year.
+SEASON_OPTIONS = (
+    ('calendar', "Single Calendar Year"),
+    ('multiple', "Span Multiple Years"),
+)
+
 # Create your models here.
 class Sport(models.Model):
     objects = models.Manager()
@@ -28,6 +34,26 @@ class Competition(models.Model):
     sport = models.ForeignKey(Sport, related_name='competitions', on_delete=models.CASCADE)
     country = models.ForeignKey(Country, related_name='competitions', on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
+   
+    def __str__(self):
+        return self.name
+        
+        
+class Season(models.Model):
+    objects = models.Manager()
+    name = models.CharField(max_length=10)
+    type = models.CharField(max_length=10, choices=SEASON_OPTIONS)
+   
+    def __str__(self):
+        return self.name
+
+
+class Edition(models.Model):
+    objects = models.Manager()
+    name = models.CharField(max_length=50)
+    competition = models.ForeignKey(Competition, related_name='editions', on_delete=models.CASCADE)
+    season = models.ForeignKey(Season, related_name='editions', on_delete=models.CASCADE)
+    is_current = models.BooleanField(default=True)
    
     def __str__(self):
         return self.name

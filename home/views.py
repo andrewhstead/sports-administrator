7b04a78ce3e398f058dca174441ae0086f4eb938
Andrew Stead <andrew@andrewstead.co.uk>
2018-11-07 10:03:13 +0000

    Added skeleton league table template to site front end.

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from users.models import User
+from football.models import Edition, Club, LeagueRecord
 
 # Create your views here.
 def home_page(request):
     
     user = User.objects.get(pk=1)
     site_name = user.site_name
+    
+    edition = Edition.objects.get(pk=34)
+    clubs = LeagueRecord.objects.filter(edition=edition).order_by('-total_points', '-table_tiebreaker', '-total_for', 'full_name')
     
     primary_color = user.primary_color
     secondary_color = user.secondary_color
@@ -17,5 +21,6 @@ def home_page(request):
         'primary_color': primary_color, 
         'secondary_color': secondary_color, 
         'primary_text': primary_text, 
-        'secondary_text': secondary_text
+        'secondary_text': secondary_text,
+        'clubs': clubs
     })
